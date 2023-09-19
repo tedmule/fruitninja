e670e1e027049cabf98b060983afd684bf6f3f8b
daddvted <daddvted@gmail.com>
2023-09-19 01:19:37 +0000

    Add K8S_API_SERVER for configuration
    Signed-off-by: daddvted <daddvted@gmail.com>

--- fruitninja/fruitninja.go
+++ fruitninja/fruitninja.go
@@ -1,13 +1,40 @@
 package fruitninja
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"k8s.io/client-go/rest"
 )
+
+var config *rest.Config = &rest.Config{
+	// Host:        "https://10.0.0.112:42883",
+	// Host:        os.Getenv("K8S_API_URL"),
+	// Host:        "https://kubernetes.default.svc",
+	BearerToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6Il8wcnFGQVcyQUs0bXdGZkhvT2ZhN01vTnRMazBnMHpDaHlHaHJBcEtqbXMifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZXYiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiZGV2LWNvbnRhaW5lci1zZWNyZXQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGV2LWNvbnRhaW5lci1zYSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjQyZmExZjBmLTVhZWUtNGYxMy05NDcxLWU1YTNlNTMzNDRmMiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZXY6ZGV2LWNvbnRhaW5lci1zYSJ9.Cv5WFQJex8nMwbs7enlcZaDsk5bIe-dHmbbsxSJVnEiyA3PAwEI_ZW9Ry87rFBLRaJAvva75Hjrel734FBZhOuBvdMlaV1SVxQG6Ll_TDY_65q0D9NS578qT9cbYqpHlVYthzZ6iK2CEsE6-9t2zzjngYGJNLeIVXfTr3tXBFHc32bx48rA-RQBF9owUfc0rfoSGLOwwvyQ__hIxhUDJQSb63xQazQyox8zpAPjBXQXuTBD_igXlak932hN2MZsDK9WdCgU-Jvf8N4xEfSrcCFClwO8N8yQ7fvpuyuhH7II6Z7TBnlyYoZub7OOjq51Kt7nkSHxvcirU2QM7zEi_mw",
+	TLSClientConfig: rest.TLSClientConfig{
+		Insecure: true,
+	},
+}
+
+func init() {
+	k8sAPI := os.Getenv("K8S_API_SERVER")
+	if strings.TrimSpace(k8sAPI) == "" {
+		k8sAPI = "https://kubernetes.default.svc"
+	}
+
+	config.Host = k8sAPI
+
+	fmt.Printf("%+v\n", config)
+}
 
 func FruitninjaSetup() *echo.Echo {
 	e := echo.New()
 
-	// e.Use(middleware.Logger())
+	e.Use(middleware.Logger())
 
 	e.GET("/", getFruitHandler)
 	e.GET("/plenty", getPlentyOfFruitHandler)
--- fruitninja/k8scli.go
+++ fruitninja/k8scli.go
@@ -2,29 +2,21 @@ package fruitninja
 
 import (
 	"context"
-	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
-
-var config *rest.Config = &rest.Config{
-	// Host:        "https://10.0.0.112:42883",
-	Host:        os.Getenv("K8S_API"),
-	BearerToken: "ZXlKaGJHY2lPaUpTVXpJMU5pSXNJbXRwWkNJNklsOHdjbkZHUVZjeVFVczBiWGRHWmtodlQyWmhOMDF2VG5STWF6Qm5NSHBEYUhsSGFISkJjRXRxYlhNaWZRLmV5SnBjM01pT2lKcmRXSmxjbTVsZEdWekwzTmxjblpwWTJWaFkyTnZkVzUwSWl3aWEzVmlaWEp1WlhSbGN5NXBieTl6WlhKMmFXTmxZV05qYjNWdWRDOXVZVzFsYzNCaFkyVWlPaUprWlhZaUxDSnJkV0psY201bGRHVnpMbWx2TDNObGNuWnBZMlZoWTJOdmRXNTBMM05sWTNKbGRDNXVZVzFsSWpvaVpHVjJMV052Ym5SaGFXNWxjaTF6WldOeVpYUWlMQ0pyZFdKbGNtNWxkR1Z6TG1sdkwzTmxjblpwWTJWaFkyTnZkVzUwTDNObGNuWnBZMlV0WVdOamIzVnVkQzV1WVcxbElqb2laR1YyTFdOdmJuUmhhVzVsY2kxellTSXNJbXQxWW1WeWJtVjBaWE11YVc4dmMyVnlkbWxqWldGalkyOTFiblF2YzJWeWRtbGpaUzFoWTJOdmRXNTBMblZwWkNJNklqUXlabUV4WmpCbUxUVmhaV1V0TkdZeE15MDVORGN4TFdVMVlUTmxOVE16TkRSbU1pSXNJbk4xWWlJNkluTjVjM1JsYlRwelpYSjJhV05sWVdOamIzVnVkRHBrWlhZNlpHVjJMV052Ym5SaGFXNWxjaTF6WVNKOS5DdjVXRlFKZXg4bk13YnM3ZW5sY1phRHNrNWJJZS1kSG1iYnN4U0pWbkVpeUEzUEF3RUlfWlc5Unk4N3JGQkxSYUpBdnZhNzVIanJlbDczNEZCWmhPdUJ2ZE1sYVYxU1Z4UUc2TGxfVERZXzY1cTBEOU5TNTc4cVQ5Y2JZcXBIbFZZdGh6WjZpSzJDRXNFNi05dDJ6empuZ1lHSk5MZUlWWGZUcjN0WEJGSGMzMmJ4NDhyQS1SUUJGOW93VWZjMHJmb1NHTE93d3Z5UV9faEl4aFVESlFTYjYzeFFhelF5b3g4enBBUGpCWFFYdVRCRF9pZ1hsYWs5MzJoTjJNWnNESzlXZENnVS1KdmY4TjR4RWZTcmNDRkNsd084Tjh5UTdmdnB1eXVoSDdJSTZaN1RCbmx5WW9adWI3T09qcTUxS3Q3bmtTSHh2Y2lyVTJRTTd6RWlfbXc=",
-	TLSClientConfig: rest.TLSClientConfig{
-		Insecure: true,
-	},
-}
 
 func getK8SService(namespace string) []string {
 	svcs := []string{}
 
 	// config, err := rest.InClusterConfig()
 	// if err != nil {
-	// 	panic(err.Error())
+	// 	fmt.Println("===============================")
+	// 	fmt.Println("error")
+	// 	// panic(err.Error())
 	// }
+
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
